4eb3a8bb34ed542850a8b1860abe6f7ff43ca1d9
André Morais <root>
2018-09-03 15:11:48 -0300

    firebase first try

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyBgv27BWNlVAINmzzQNIJZvQWngSynf4-o",
+  authDomain: "react-firebase-login-19757.firebaseapp.com",
+  databaseURL: "https://react-firebase-login-19757.firebaseio.com",
+  projectId: "react-firebase-login-19757",
+  storageBucket: "react-firebase-login-19757.appspot.com",
+  messagingSenderId: "75747015336"
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
